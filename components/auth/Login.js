e134691b26b460405ffb42bc6a9a5c9d21cbdf3b
royne <royne.chaves@gmail.com>
2022-04-14 22:19:37 -0500

    connect login

--- components/auth/Login.js
+++ components/auth/Login.js
@@ -1,8 +1,15 @@
 import React, { useState, useEffect } from 'react'
 import Modal from '../layout/Modal'
+import { useUser } from "../../context/userContext";
+import { login } from "../../settings/auth";
 
 const Login = ({ openModal, hideModal}) => {
   const [showModal, setShowModal] = useState(false);
+  const [data, setData] = useState({
+    email: "",
+    password: ""
+  });
+  const { setChargeUser, setUser } = useUser();
 
   const closeModal = () => {
     setShowModal(false)
@@ -13,16 +20,37 @@ const Login = ({ openModal, hideModal}) => {
     setShowModal(openModal)
   }, [openModal])
 
+  const handleChange = e => {
+    setData({
+      ...data,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const submitForm = async e => {
+    e.preventDefault()
+    const dataUser = await login(data);
+    if (dataUser) {
+      setUser(dataUser);
+      setChargeUser(false);
+    } else {
+      closeModal()
+      alert("algo salio mal")
+      return
+    }
+    closeModal();
+  }
+
   return (
     <Modal onClose={closeModal} show={showModal}>
-      <form>
+      <form onSubmit={submitForm}>
         <div>
           <label>Email</label>
-          <input type="mail" />
+          <input type="mail" name="email" onChange={handleChange}/>
         </div>
         <div>
           <label>Password</label>
-          <input type="password" />
+          <input type="password" name="password" onChange={handleChange}/>
         </div>
         <div>
           <input type="submit" value="SIGN IN" />
--- components/layout/Header.js
+++ components/layout/Header.js
@@ -1,10 +1,12 @@
 import React, {useState} from 'react'
+import {useUser} from '../../context/userContext'
 import { HeaderNav, HeaderBox, HeaderNavBottom } from "../ui/home/Header";
 import Link from 'next/link';
 import Login from '../auth/Login'
 
 const Header = () => {
   const [showLogin, setShowLogin] = useState(false)
+  const { user } = useUser()
   const showModal = () => setShowLogin(true)
   const hideModal = (elm) => setShowLogin(elm)
   
@@ -12,7 +14,7 @@ const Header = () => {
     <>
       <HeaderNav>
         <img src="/menu.png" />
-        <img src="/usuario.png" onClick={showModal}/>
+        {user ? <img src="/usuario.png" /> : <span onClick={showModal}>Iniciar sesion</span>}
       </HeaderNav>
       <HeaderBox>
         <HeaderNavBottom />
