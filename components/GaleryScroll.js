8a46a0f6a8d44f1c65ec56b6cbd4d0f61a8ba49e
royne <royne.chaves@gmail.com>
2022-04-26 14:45:29 -0500

    change request for isg

--- components/GaleryScroll.js
+++ components/GaleryScroll.js
@@ -1,19 +1,8 @@
-import { useEffect, useState } from 'react'
-import { BASE_URL } from '../settings/base'
+import {useState} from 'react' 
 import CardScroll from '../components/layout/CardScroll'
 
-const GaleryScroll = () => {
-  const [escorts, setScorts] = useState([])
+const GaleryScroll = ({ escorts }) => {
   const [changeGrid, setChangeGrid] = useState(false)
-
-  useEffect(() => {
-    const getData = async () => {
-      const response = await fetch(`${BASE_URL}/escorts/randon_premium`)
-      const data = await response.json()
-      setScorts(data)
-    }
-    getData()
-  }, [])
   return (
     <>
       {escorts && escorts.map(elm => <CardScroll key={elm.photo} escort={elm} photo={elm.photo} setChangeGrid={setChangeGrid} gridHome={true}/>)}
--- pages/premium_slides.js
+++ pages/premium_slides.js
@@ -1,13 +1,24 @@
-import React from 'react'
+import {BASE_URL} from '../settings/base'
 import GaleryScroll from '../components/GaleryScroll'
 import Layout from '../components/layout/Layout'
 
-const premium_slides = () => {
+const premium_slides = ({ escorts }) => {
   return (
     <Layout showBanner={false}>
-      <GaleryScroll />
+      <GaleryScroll escorts={escorts} />
     </Layout>
   )
 }
 
 export default premium_slides
+
+export async function getStaticProps({ params }) {
+  const response = await fetch(`${BASE_URL}/escorts/randon_premium`)
+  const escorts = await response.json()
+  return {
+    props: {
+      escorts
+    },
+    revalidate: 3600, // In seconds
+  }
+}
