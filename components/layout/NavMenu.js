fadd55254b1a462ec4e550938023ed09bc724fda
jhon11390 <meljhon2@gmail.com>
2022-04-29 05:03:01 -0500

    create component search

--- components/layout/NavMenu.js
+++ components/layout/NavMenu.js
@@ -1,18 +1,57 @@
+import React, {useState} from 'react'
 import Link from 'next/link'
+import FilterSearch from '../layout/Filter'
 import {ContainerNavMenu} from '../ui/home/NavMenu'
 import { deleteToken } from '../../settings/auth'
 import {useUser} from '../../context/userContext'
+import { MenuNav, MenuInBlackUp, MenuNewUp} from "../ui/home/Menu_emergent"
+
 
 const NavMenu = () => {
+  const [showMenu, setShowMenu] = useState(false)
   const { setUser } = useUser()
   const logOut = () => {
     deleteToken()
     setUser(null)
   }
+
+  const openMenu = () => {
+    if(showMenu){
+      setShowMenu(false)
+    }else{
+      setShowMenu(true)
+    }
+  }
+
+  const closeMenu = (e) => {
+    if(e.target.classList.contains("menuBlack")){
+      setShowMenu(false)
+    }
+  }
+
   return (
     <ContainerNavMenu>
       <Link href="/premium_slides"><img src="/icons/active.svg"/></Link>
-      <Link href="/"><img src="/icons/search.svg"/></Link>
+      
+      <MenuNav>
+        <div className="icono-menu" onClick={openMenu}>
+          <img src="/icons/search.svg" id="icono-menu" width="25"/>
+        </div>
+        <MenuNewUp className="cont-menu active" id="menu" style={showMenu ? {transform: "translateY(0px)"} : {transform: "translateY(150%)"}} onClick={closeMenu}>  
+          <MenuInBlackUp className="menuBlack">
+            <h1><img src="/icons/search.svg" id="icono-menu" width="20"/> Buscar escort</h1>
+          </MenuInBlackUp>
+          <div>
+
+            <FilterSearch />
+
+            <div className="icono-menu" onClick={openMenu} style={{position: "absolute", bottom: "3%", right: "5%"}}>
+              <img src="/icons/delete.png" id="icono-menu" width="20" style={{filter: "invert(100%)"}}/>
+            </div>
+          </div>
+        </MenuNewUp>
+      </MenuNav>
+
       <Link href="/"><img src="/icons/noti.svg" onClick={logOut} /></Link>
     </ContainerNavMenu>
   )
--- components/ui/home/Menu_emergent.js
+++ components/ui/home/Menu_emergent.js
@@ -7,7 +7,10 @@ export const MenuNav = styled.nav`
 
 //menu emergente
 export const MenuNew = styled.div`
-  background-color: var(--black);
+  background: rgba(0,0,0,.98) url("/nudeMenu.jpg");
+  background-position: center;
+  background-size: cover;
+  background-blend-mode: darken;
   box-shadow: 2px 0px 5px 2px rgb(66,66,66);
   width: 70%;
   height: 100vh;
@@ -41,5 +44,33 @@ export const MenuInBlack = styled.div`
   top: 0; 
   left: 100%;
   height: 100vh; 
-  width: 43%
+  width: 43%;
+`
+
+export const MenuNewUp = styled.div`
+  background: rgba(0,0,0,.97) url("/passionSearch.jpg");
+  background-position: center;
+  background-size: cover;
+  background-blend-mode: darken;
+  box-shadow: 2px 0px 5px 2px rgb(66,66,66);
+  width: 100%;
+  height: 70vh;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  z-index: 10;
+  transition: all .5s ease;
+  
+`
+
+export const MenuInBlackUp = styled.div`
+  position: absolute; 
+  background: var(--black);
+  opacity: .8;
+  width: 100%;
+  height: 30vh;
+  bottom: 100%; 
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `
